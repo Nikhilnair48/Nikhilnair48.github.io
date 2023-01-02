d47d316e3c0e633b921ea4e336b9eb37ccef89a0
Nikhil Nair <nammazhwar@Nikhils-MacBook-Pro.local>
2023-01-02 09:30:07 +0530

    chore: Configuring github pages script

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,6 +9,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
+  pathPrefix: "/Nikhilnair48.github.io",
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sharp", {
     resolve: `gatsby-source-filesystem`,
     options: {
